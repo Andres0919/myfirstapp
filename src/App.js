82187709811401ef3155ee20e03266cd0530eada
Andres0919 <andres.posada0919@gmail.com>
2018-10-25 21:37:13 -0500

    creating react components

--- src/App.js
+++ src/App.js
@@ -2,25 +2,36 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { todos } from './todos.json'
+import Navigation from './components/Navigation';
+
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      todos
+    }
+  }
+
   render() {
+    const todos = this.state.todos.map((todo, i) => {
+      return (
+        <div className="card">
+          <div className="card-header">
+            <h3> { todo.title } </h3>
+          </div>
+        </div>
+      )
+    });
     return (
       <div className="App">
+        <Navigation title="My first navigation" />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <h1>This is my first app with react, do you like it?</h1>
+          <h1 className="display-1">This is my first app with react, do you like it?</h1>
         </header>
+        { todos }
       </div>
     );
   }
